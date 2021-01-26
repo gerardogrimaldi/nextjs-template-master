031ff1eae8d70033d845e80fa336fba0a26dd8a8
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2021-01-26 11:33:47 -0300

    ADDED footer and headbar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,117 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import {ServerStyleSheets, ThemeProvider} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Layout from '@/components/layout';
-import theme from '@/components/theme';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Footer from "../components/footer";
+import { useRouter } from 'next/router'
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
+import HomeIcon from '@material-ui/icons/Home';
+
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
+  const router = useRouter();
+
+  const drawerWidth = 240;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexGrow: 1,
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(1),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+    title: {
+      flexGrow: 1,
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block"
+      }
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+  }));
+  const sheets = new ServerStyleSheets();
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -19,17 +123,114 @@ export default function MyApp(props) {
 
   return (
     <React.Fragment>
-      <Layout>
+      <Head>
+        <title>New Web</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography className={classes.title} variant="h6" noWrap>
+                DODICI
+              </Typography>
+              {/*<SearchBox></SearchBox>*/}
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button key={0}
+                        onClick={() => router.push('/')}>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Home'} />
+              </ListItem>
+              <ListItem button key={1}
+                        onClick={() => router.push('/request')}>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Pedido'} />
+              </ListItem>
+              <ListItem button key={2}
+                        onClick={() => router.push('/player')}>
+                <ListItemIcon>
+                  <DirectionsRunIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Jugador'} />
+              </ListItem>
+              <ListItem button key={3}
+                        onClick={() => router.push('/club')}>
+                <ListItemIcon>
+                  <SportsSoccerIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Clubes'} />
+              </ListItem>
+            </List>
+            <Divider />
+            {/*<List>
+              {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>*/}
+          </Drawer>
+          <main
+            className={clsx(classes.content, {
+              [classes.contentShift]: open,
+            })}
+          >
+            <div className={classes.drawerHeader} />
+            <Component {...pageProps} />
+          </main>
+        </div>
+        <Footer/>
+      </ThemeProvider>
+
+      {/*<Layout>
         <Head>
           <title>Next.js + MongoDB App</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+         CssBaseline kickstart an elegant, consistent, and simple baseline to build upon.
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-      </Layout>
+      </Layout>*/}
     </React.Fragment>
   );
 }
