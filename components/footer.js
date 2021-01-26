45884b4976c62fe43e85eff770b1375d97cfdc78
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2021-01-26 11:33:40 -0300

    ADDED footer and headbar

---
+++ components/footer.js
@@ -0,0 +1,41 @@
+import Typography from "@material-ui/core/Typography";
+import {makeStyles, ThemeProvider} from "@material-ui/core/styles";
+import React from "react";
+import Link from "@material-ui/core/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © FWTV'}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  })
+);
+
+export default function Footer(props) {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer FWTV
+      </Typography>
+      <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        Something here to give the footer a purpose!
+      </Typography>
+      <Copyright/>
+    </footer>
+  )
+}
